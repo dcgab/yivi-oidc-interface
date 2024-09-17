53f01ea29bf04dee9cc654cc6fa1aceccd51add8
Dennis <dc.gabeler@gmail.com>
2024-09-17 11:34:15 +0000

    Updated Hydra client script

--- yivi-consent-node/scripts/create_client_prod.sh
+++ yivi-consent-node/scripts/create_client_prod.sh
@@ -7,7 +7,7 @@ code_client=$(docker compose exec hydra \
     --response-type code,id_token \
     --format json \
     --scope openid --scope pbdf.sidn-pbdf.email.email --scope pbdf.sidn-pbdf.mobilenumber.mobilenumber --scope pbdf.gemeente.personalData.firstnames --scope pbdf.gemeente.personalData.familyname \
-    --redirect-uri http://127.0.0.1:4433/self-service/methods/oidc/callback/yivi \
+    --redirect-uri https://demo-app.dennisgabeler.nl/self-service/methods/oidc/callback/yivi \
     --logo-uri https://www.ru.nl/themes/custom/ru/logo.svg \
     --name "Radboud Universiteit")
 
